720c44161f48f019e36a91bb54b7af9327737a45
RandIsm20 <engrandismail@gmail.com>
2024-01-26 21:52:08 +0200

    Trello

--- cypress/pageObjects/hideTemplate/assertions.cy.js
+++ cypress/pageObjects/hideTemplate/assertions.cy.js
@@ -1,10 +1,11 @@
 class hideTemplateAssertions {
-  checkCardHided() {
+  checkCardHided(){
     cy.get("[data-testid='list-cards']").eq(0).should("be.empty");
     cy.wait(1000);
     cy.get(".board-header [data-testid='OverflowMenuHorizontalIcon']").click();
     cy.get("a").contains("Archived items").click();
     cy.get("[data-testid='card-name']").should("be.visible");
+    cy.get(".board-menu-header-close-button").click();
   }
 }
 
